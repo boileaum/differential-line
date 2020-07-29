f1cd5d48d3416b057006290c3b5f55dbd06d2fd3
Matthieu Boileau <matthieu.boileau@math.unistra.fr>
2020-07-29 11:41:33 +0200

    main_line.py: autopep8 + diagonal line

--- main_line.py
+++ main_line.py
@@ -6,7 +6,8 @@ from numpy.random import random
 from modules.growth import spawn, spawn_curl
 
 NMAX = 10**7
-SIZE = 10000
+#SIZE = 10000
+SIZE = 5000
 ONE = 1./SIZE
 
 STP = ONE*0.02
@@ -21,103 +22,98 @@ LINEWIDTH = 5.*ONE
 
 INIT_NUM = 7
 
-BACK = [1,1,1,1]
-FRONT = [0,0,0,0.08]
+BACK = [1, 1, 1, 1]
+FRONT = [0, 0, 0, 0.08]
 
 TWOPI = pi*2.
 
 
 def main():
 
-  from time import time
-  from itertools import count
+    from time import time
+    from itertools import count
 
-  from differentialLine import DifferentialLine
+    from differentialLine import DifferentialLine
 
-  from iutils.render import Render
-  from modules.helpers import print_stats
+    from iutils.render import Render
+    from modules.helpers import print_stats
 
-  from modules.show import sandstroke
-  from modules.show import show
-  from modules.show import dots
+    from modules.show import sandstroke
+    from modules.show import show
+    from modules.show import dots
 
+    np_coords = zeros(shape=(NMAX, 4), dtype='float')
+    np_vert_coords = zeros(shape=(NMAX, 2), dtype='float')
 
-  np_coords = zeros(shape=(NMAX,4), dtype='float')
-  np_vert_coords = zeros(shape=(NMAX,2), dtype='float')
+    DF = DifferentialLine(NMAX, FARL*2, NEARL, FARL, PROCS)
 
+    render = Render(SIZE, BACK, FRONT)
 
-  DF = DifferentialLine(NMAX, FARL*2, NEARL, FARL, PROCS)
+    render.ctx.set_source_rgba(*FRONT)
+    render.ctx.set_line_width(LINEWIDTH)
 
-  render = Render(SIZE, BACK, FRONT)
+    # angles = sorted(random(INIT_NUM)*TWOPI)
+    # DF.init_circle_segment(MID,MID,0.2, angles)
 
-  render.ctx.set_source_rgba(*FRONT)
-  render.ctx.set_line_width(LINEWIDTH)
+    # arc
 
-  # angles = sorted(random(INIT_NUM)*TWOPI)
-  # DF.init_circle_segment(MID,MID,0.2, angles)
+    # angles = sorted(random(INIT_NUM)*pi*1.5)
+    # xys = []
+    # for a in angles:
+    #   x = 0.5 + cos(a)*0.2
+    #   y = 0.5 + sin(a)*0.2
+    #   xys.append((x,y))
 
-  ## arc
+    # DF.init_line_segment(xys, lock_edges=1)
 
-  angles = sorted(random(INIT_NUM)*pi*1.5)
-  xys = []
-  for a in angles:
-    x = 0.5 + cos(a)*0.2
-    y = 0.5 + sin(a)*0.2
-    xys.append((x,y))
+    # vertical line
 
-  DF.init_line_segment(xys, lock_edges=1)
+    # yy = sorted(MID + 0.2*(1-2*random(INIT_NUM)))
+    # xx = MID+0.005*(0.5-random(INIT_NUM))
+    # xys = []
+    # for x, y in zip(xx, yy):
+    #     xys.append((x, y))
 
-  ## vertical line
+    # DF.init_line_segment(xys, lock_edges=1)
 
-  #yy = sorted(MID + 0.2*(1-2*random(INIT_NUM)))
-  #xx = MID+0.005*(0.5-random(INIT_NUM))
-  #xys = []
-  #for x,y in zip(xx,yy):
-    #xys.append((x,y))
+    # diagonal line
 
-  #DF.init_line_segment(xys, lock_edges=1)
+    yy = sorted(MID + 0.2*(1-2*random(INIT_NUM)))
+    xx = sorted(MID + 0.2*(1-2*random(INIT_NUM)))
+    xys = []
+    for x, y in zip(xx, yy):
+        xys.append((x, y))
 
-  ## diagonal line
+    DF.init_line_segment(xys, lock_edges=1)
 
-  # yy = sorted(MID + 0.2*(1-2*random(INIT_NUM)))
-  # xx = sorted(MID + 0.2*(1-2*random(INIT_NUM)))
-  # xys = []
-  # for x,y in zip(xx,yy):
-    # xys.append((x,y))
+    for i in count():
 
-  # DF.init_line_segment(xys, lock_edges=1)
+        t_start = time()
 
+        DF.optimize_position(STP)
+        spawn_curl(DF, NEARL, 0.016)
 
-  for i in count():
+        if i % 100 == 0:
+            fn = './res/chris_bd_{:04d}.png'.format(i)
+        else:
+            fn = None
 
-    t_start = time()
+        render.set_front(FRONT)
+        num = DF.np_get_edges_coordinates(np_coords)
+        #sandstroke(render, np_coords[:num, :], 20, fn)
 
-    DF.optimize_position(STP)
-    spawn_curl(DF,NEARL,0.016)
+        if random() < 0.05:
+            sandstroke(render, np_coords[:num, :], 30, None)
 
-    if i%100==0:
-      fn = './res/chris_bd_{:04d}.png'.format(i)
-    else:
-      fn = None
+        vert_num = DF.np_get_vert_coordinates(np_vert_coords)
+        #dots(render, np_vert_coords[:vert_num, :], None)
+        dots(render, np_vert_coords[:vert_num, :], fn)
 
-    render.set_front(FRONT)
-    num = DF.np_get_edges_coordinates(np_coords)
-    sandstroke(render,np_coords[:num,:],20,fn)
+        t_stop = time()
 
-
-    if random()<0.05:
-      sandstroke(render,np_coords[:num,:],30,None)
-
-    vert_num = DF.np_get_vert_coordinates(np_vert_coords)
-    dots(render,np_vert_coords[:vert_num,:],None)
-
-
-    t_stop = time()
-
-    print_stats(i,t_stop-t_start,DF)
+        print_stats(i, t_stop-t_start, DF)
 
 
 if __name__ == '__main__':
 
     main()
-
